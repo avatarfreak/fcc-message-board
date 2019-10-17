ad2e398f6cb62f12561103dc4e30d1a8b6d7ef02
avatarfreak <avatarfreak>
2019-10-17 16:48:06 +0530

    refactor

--- controller/ReplyController.js
+++ controller/ReplyController.js
@@ -4,26 +4,19 @@ const Reply = require("../model/replyModel");
 
 //POST
 const postReplies = async (req, res) => {
-  //Getting all the name field from form
-  const keys = Object.keys(req.body);
+  const keys = Object.keys(req.body); //Getting all the name field from the form
+  const { thread_id, text } = req.body;
 
   const allowedUpdates = ["board", "thread_id", "text", "delete_password"];
-  //Only valid name field is allowed, if no then return false
-  const isValidfield = keys.every(key => allowedUpdates.includes(key));
+  const isValidfield = keys.every(key => allowedUpdates.includes(key)); //Only valid field from the from is allowed, if no then return false
 
-  if (!isValidfield) {
-    return res.status(401).send("error: Invalid field");
-  }
-  const { thread_id, text } = req.body;
-  if (!mongoose.Types.ObjectId.isValid(thread_id) || !text) {
+  if (!isValidfield) return res.status(401).send("error: Invalid field");
+
+  if (!mongoose.Types.ObjectId.isValid(thread_id) || !text)
     return res.status(401).send("Please provide the correct information.");
-  }
-  if (!keys.includes("board")) {
-    req.body = {
-      ...req.body,
-      board: req.params.board
-    };
-  }
+
+  if (!keys.includes("board"))
+    req.body = { ...req.body, board: req.params.board };
 
   const replies = new Reply({
     ...req.body,
@@ -52,18 +45,18 @@ const postReplies = async (req, res) => {
 const getReplies = async (req, res) => {
   const { thread_id } = req.query;
 
-  if (!mongoose.Types.ObjectId.isValid(thread_id)) {
+  if (!mongoose.Types.ObjectId.isValid(thread_id))
     return res.status(404).send("Invalid credential provided.");
-  }
-
-  const query = { thread: thread_id };
-  const update = { delete_password: 0, reported: 0 };
 
   try {
-    const replies = await Reply.find(query, update).populate({
+    const query = { thread: thread_id };
+    const excludes = { reported: 0, delete_password: 0 };
+    const replies = await Reply.find(query, excludes).populate({
       path: "thread",
+      select: excludes,
       options: { sort: { created_on: -1 } }
     });
+
     res.status(200).send({ replies });
   } catch (error) {
     res.status(400).send(error);
@@ -75,22 +68,16 @@ const putReplies = async (req, res) => {
   const keys = Object.keys(req.body);
   const allowedUpdates = ["board", "thread_id", "reply_id"];
   const isValidField = keys.every(key => allowedUpdates.includes(key));
-
-  if (!isValidField) {
-    return res.status(401).send("error: Invalid field");
-  }
-
-  if (!keys.includes("board")) {
-    req.body = {
-      ...req.body,
-      board: req.params.board
-    };
-  }
   const { reply_id, thread_id } = req.body;
   const isValid = mongoose.Types.ObjectId.isValid;
-  if (!isValid(reply_id) || !isValid(thread_id)) {
+
+  if (!isValidField) return res.status(401).send("error: Invalid field");
+
+  if (!keys.includes("board"))
+    req.body = { ...req.body, board: req.params.board };
+
+  if (!isValid(reply_id) || !isValid(thread_id))
     return res.send("Invalid information provided.");
-  }
 
   try {
     await Reply.findByIdAndUpdate(reply_id, { $set: { reported: true } });
@@ -102,30 +89,30 @@ const putReplies = async (req, res) => {
 
 //DELETE
 const deleteReplies = async (req, res) => {
-  const { board, thread_id, reply_id, delete_password } = req.body;
-  if (!req.body.hasOwnProperty("board")) {
-    req.body = { ...req.body, board: req.params.board };
-  }
-
+  const { thread_id, reply_id, delete_password } = req.body;
   const isValid = mongoose.Types.ObjectId.isValid;
-  if (!isValid(reply_id) || !isValid(thread_id)) {
-    return res.status(200).send("Invalid ID provided.");
-  }
-  try {
-    await Reply.findOne({ _id: reply_id }).exec((err, data) => {
-      if (err) throw err;
 
-      if (data.delete_password !== delete_password) {
-        return res.send("incorrect password");
-      }
-      data.text = "[deleted]";
-      data.save();
-      return res.send("success");
-    });
+  if (!req.body.hasOwnProperty("board"))
+    req.body = { ...req.body, board: req.params.board };
+
+  if (!isValid(reply_id) || !isValid(thread_id))
+    return res.status(200).send("Invalid ID provided.");
+
+  try {
+    const doc = await Reply.findById(reply_id);
+    if (!doc) return res.send("Invalid ID provided.");
+
+    if (doc.delete_password !== delete_password)
+      return res.send("incorrect password");
+
+    doc.text = "[deleted]";
+    doc.save();
+    res.send("success");
   } catch (error) {
     res.status(400).send(error);
   }
 };
+
 module.exports = {
   getReplies,
   postReplies,
